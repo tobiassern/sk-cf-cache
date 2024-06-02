863e47bdfcdb67af2dca0869e732e9d409772910
Tobias <tobias@sernhede.com>
2024-06-02 15:36:04 +0200

    x

--- src/routes/revalidate/+server.ts
+++ src/routes/revalidate/+server.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 export const GET: RequestHandler = async (event) => {
-	await event.platform?.caches.delete(event.url.searchParams.get('path') ?? '/');
+	const cache = event.platform?.caches.default;
+	await cache?.delete(event.url.searchParams.get('path') ?? '/');
 	return json({ success: true });
 };
